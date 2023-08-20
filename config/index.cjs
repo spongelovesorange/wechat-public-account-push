1ffc303a5e1da3d98974a865cbb64ae5d4e2a945
spongelovesorange <142749375+spongelovesorange@users.noreply.github.com>
2023-08-21 03:50:02 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -13,13 +13,13 @@ USE_PASSAGE: 'push-deer',
   // 使用微信测试号：公众号APP_SECRET
   APP_SECRET: '',
 
-  PROVINCE: '广东',
-  CITY: '惠州',
+  PROVINCE: '江苏',
+  CITY: '苏州',
 
   USERS: [
     {
       // 想要发送的人的名字
-      name: '宝贝',
+      name: '灿娜娜',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'PDU24946TDl1Vtrz9keU96clU3BZ68JZx84sObo3V',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
@@ -29,22 +29,21 @@ USE_PASSAGE: 'push-deer',
       festivals: [
         // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
         {
-          type: '*生日', name: '宝贝', year: '1996', date: '09-09',
+          type: '*生日', name: '灿娜娜', year: '2004', date: '02-17',
         },
         // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
         {
           type: '生日', name: '李四', year: '1996', date: '09-31',
         },
         {
-          type: '节日', name: '相识纪念日', year: '2020', date: '09-03',
+          type: '节日', name: '相识纪念日', year: '2023', date: '03-28',
         },
       ],
       // 我们在一起已经有xxxx天了的配置
       customizedDateList: [
         // 在一起的日子
-        { keyword: 'love_day', date: '2022-09-08' },
-        // 结婚纪念日
-        { keyword: 'marry_day', date: '2022-09-09' },
+        { keyword: 'love_day', date: '2023-03-28' },
+  
       ],
     },
   ],
@@ -55,7 +54,7 @@ USE_PASSAGE: 'push-deer',
 
   CALLBACK_USERS: [
     {
-      name: '自己',
+      name: '邱释',
       // 使用微信测试号：自己的微信id，扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: '',
     }
