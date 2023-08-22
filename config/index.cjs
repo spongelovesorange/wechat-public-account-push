d485eccc2543cb72e03ded3234b1066db040f235
spongelovesorange <142749375+spongelovesorange@users.noreply.github.com>
2023-08-23 03:41:47 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -21,7 +21,7 @@ USE_PASSAGE: 'push-deer',
       // 想要发送的人的名字
       name: '灿娜娜',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'oAe2i6qAut-EI8jgWOx0jIVlPA-4',
+      id: 'PDU24946Tr3EZKe3yDYZorhLZl8H7bDZFP8o2Xpd7',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       useTemplateId: '0001',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
@@ -48,7 +48,7 @@ USE_PASSAGE: 'push-deer',
 
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: 'WJtGKAFnI8OIvoNicesMR__rMelPTkbmqjso7tJ9n-g',
+  CALLBACK_TEMPLATE_ID: '0002',
 
   CALLBACK_USERS: [
     {
